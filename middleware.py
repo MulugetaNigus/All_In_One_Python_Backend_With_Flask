587d899ee5105d33cb455f03092c578df5f0c75c
muller king <mullerhihi@gmail.com>
2025-09-30 07:24:39 -0700

    update middlware secret values

--- middleware.py
+++ middleware.py
@@ -1,12 +1,15 @@
 from flask import request
 import jwt
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def middleware():
     try:
         token = request.headers.get("Authorization")
         if not token:
             return jsonify({"error": "token is required"}), 401
-        jwt.decode(token, "someofmysecretvalueshere", algorithms=["HS256"])
+        jwt.decode(token, os.environ.get("JWT_SECRET_KEY"), algorithms=["HS256"])
         return True
     except Exception as e:
         return jsonify({"error": "token is invalid"}), 401
