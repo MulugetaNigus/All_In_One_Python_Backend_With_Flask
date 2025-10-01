bcdb1caaf8f9916754b5cb2524a61ea0338721f8
muller king <mullerhihi@gmail.com>
2025-10-01 12:38:13 -0700

    add api versioning controller

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from flask_limiter.util import get_remote_address
 # init flask app
 app = Flask(__name__)
 
+# api versioning
+API_VERSION = "api/v1"
+
 # init limiter
 limiter = Limiter(
     get_remote_address,
@@ -20,54 +23,54 @@ def auth_middleware():
     return middleware()
 
 # auth route
-@app.route("/register", methods=["POST"])
+@app.route(f"/{API_VERSION}/register", methods=["POST"])
 @limiter.limit("3 per minute")
 def Register():
     return register()
 
-@app.route("/login", methods=["POST"])
+@app.route(f"/{API_VERSION}/login", methods=["POST"])
 @limiter.limit("3 per minute")
 def Login():
     return login()
 
-@app.route("/signout", methods=["POST"])
+@app.route(f"/{API_VERSION}/signout", methods=["POST"])
 def SignOut():
     return signOut()
 
-@app.route("/send-reset-password", methods=["POST"])
+@app.route(f"/{API_VERSION}/send-reset-password", methods=["POST"])
 @limiter.limit("3 per minute")
 def SendResetPassword():
     return sendResetPassword()
 
-@app.route("/change-password", methods=["POST"])
+@app.route(f"/{API_VERSION}/change-password", methods=["POST"])
 @limiter.limit("3 per minute")
 def ChangePassword():
     return changePassword()
 
 
 # get all prouct route
-@app.route("/product", methods=["GET"])
+@app.route(f"/{API_VERSION}/product", methods=["GET"])
 def Product():
     return getProduct()
 
 # add product route
-@app.route("/product", methods=["POST"])
+@app.route(f"/{API_VERSION}/product", methods=["POST"])
 @limiter.limit("15 per minute")
 def AddProduct():
     return addProduct()
 
 # update product route
-@app.route("/update-product", methods=["PUT"])
+@app.route(f"/{API_VERSION}/update-product", methods=["PUT"])
 def UpdateProduct():
     return updateProduct()
 
 # delete product route
-@app.route("/delete", methods=["DELETE"])
+@app.route(f"/{API_VERSION}/delete", methods=["DELETE"])
 def DeleteProduct():
     return deleteProduct()
 
 # filter the product
-@app.route("/filter-product", methods=["POST"])
+@app.route(f"/{API_VERSION}/filter-product", methods=["POST"])
 def FilterProduct():
     return filterProduct()
 
